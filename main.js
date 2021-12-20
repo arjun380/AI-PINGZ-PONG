646751d5a8bea332e6e8587f80cb2b452307f42d
arjunTheCoder <82092827+arjun380@users.noreply.github.com>
2021-12-20 19:56:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -27,6 +27,23 @@ function setup(){
   video = createCapture(VIDEO);
   video.size(700, 600);
   video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+  console.log('Model Loaded!');
+  
+}
+
+function gotPoses(results){
+if(results.length>0)
+  {
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log("noseX = "+ noseX +", noseY="+noseY);
+  }
 }
 
 
